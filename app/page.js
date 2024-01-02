b475f9d69e8f2f240de0a7ff0dc544f7ae1dd032
vaxad <varadprabhu111@gmail.com>
2024-01-02 13:56:52 +0530

    added new portfolio link

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 "use client"
 
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Projects from "./components/Projects";
@@ -8,9 +8,19 @@ import Lines from "./components/Lines";
 import Form from "./components/Form";
 import Achievements from "./components/Achievements";
 import Experiences from "./components/Experiences";
+import Toast from "./components/Toast"
 
 export default function Main() {
   const hiddenRef = useRef();
+  const [toast, setToast] = useState(false)
+  const [text, setText] = useState("Hello visitor!")
+  const [error,setError] = useState(false)
+  useEffect(() => {
+    setText("Explore my most recent portfolio, this one is outdated!")
+    setError(true)
+    setToast(true)
+  }, [])
+  
   return (
     <>
     <div className='static-overlay flex w-full '>
@@ -18,6 +28,7 @@ export default function Main() {
       <Lines/>
       <Navbar hiddenRef={hiddenRef}/>
       <Home/>
+      <Toast toast={toast} text={text} setToast={setToast} error={error}/>
       <Projects hiddenRef={hiddenRef}/>
       <Achievements/>
       <Experiences/>
